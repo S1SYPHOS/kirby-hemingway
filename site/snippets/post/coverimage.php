0f54843cdec391644df9f3f46f2377f08420d5a4
Martin Folkers <hello@twobrain.io>
2018-01-27 20:13:57 +0100

    Improving alt attribute (accessibility)
    thx @texnixe

--- site/snippets/post/coverimage.php
+++ site/snippets/post/coverimage.php
@@ -2,7 +2,7 @@
 <div class="featured-media">
   <?php e($item->sticky()->int() == 1 && !$item->isHomePage(), '<span class="sticky-post">Sticky post</span>') ?>
   <a href="<?= $item->url() ?>" rel="bookmark" rel="bookmark" title="<?= $item->title()->html() ?>">
-    <img src="<?= $image->resize(766, null, 85)->url() ?>"<?php e($image->alt()->isNotEmpty(), ' alt="' . $image->alt()->html() . '"') ?>>
+    <img src="<?= $image->resize(766, null, 85)->url() ?>" alt="<?php e($image->alt()->isNotEmpty(), $image->alt()->html()) ?>">
     <?php if($image->caption()->isNotEmpty()) : ?>
     <div class="media-caption-container">
       <p class="media-caption">
